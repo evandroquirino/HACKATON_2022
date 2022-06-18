9a9e58779bdafde67cc116ed77eb4ae9fce18178
evandro quirino <evandroquirino@Mac-mini-de-evandro.local>
2022-06-18 12:52:51 -0300

    conecta com banco online

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('clientes')->group(function () {
-    Route::get('/', [ClienteController::class, 'index'])->name('clientes.index');
-    Route::get('/create', [ClienteController::class, 'create'])->name('clientes.create');    
-    Route::get('/{id}/edit', [ClienteController::class, 'edit'])->name('cliente.edit');
+// Route::prefix('clientes')->group(function () {
+//     Route::get('/', [ClienteController::class, 'index'])->name('clientes.index');
+//     Route::get('/create', [ClienteController::class, 'create'])->name('clientes.create');    
+//     Route::get('/{id}/edit', [ClienteController::class, 'edit'])->name('cliente.edit');
    
-    Route::post('/', [ClienteController::class, 'store'])->name('cliente.store');
-    Route::put('/{id}', [ClienteController::class, 'update'])->name('cliente.update');
-    Route::delete('/{id}', [ClienteController::class, 'destroy'])->name('cliente.destroy');
+//     Route::post('/', [ClienteController::class, 'store'])->name('cliente.store');
+//     Route::put('/{id}', [ClienteController::class, 'update'])->name('cliente.update');
+//     Route::delete('/{id}', [ClienteController::class, 'destroy'])->name('cliente.destroy');
     
-});
+// });

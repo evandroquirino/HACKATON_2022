e2d9ad4048d2f3f17f181b62a558102977ab5e63
evandroquirino <evandroquirin@gmail.com>
2022-06-25 14:43:09 -0300

    cria endpoint para listar perguntas nao respondidas

--- app/Http/Controllers/Api/ClientNoSurveysController.php
+++ app/Http/Controllers/Api/ClientNoSurveysController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Response;
 use Illuminate\Http\Request;
 
 class ClientNoSurveysController extends Controller
 {
-    //
+    public function index($client_id)
+    {
+        $surveys = Response::NoreadRespostaPesquisa($client_id);
+
+        return response()->json($surveys);
+ 
+    }
 }
--- app/Models/Response.php
+++ app/Models/Response.php
@@ -33,4 +33,13 @@ class Response extends Model
         ->where('responses.client_id', $client_id)
         ->get();
     }
+
+    public static function NoreadRespostaPesquisa($client_id) {
+        return Response::orderBy('responses.updated_at', 'desc')
+        ->join('clients', 'clients.id', '=', 'responses.client_id')
+        ->join('surveys', 'surveys.id', '=', 'responses.survey_id')
+        ->select('responses.id as id_response', 'clients.name as name_client', 'clients.id as id_client', 'surveys.title as title_survey', 'surveys.id as id_survey', 'responses.value')
+        ->whereNotin('responses.client_id', [$client_id])
+        ->get();
+    }
 }
